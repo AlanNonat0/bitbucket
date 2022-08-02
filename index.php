c757e3e4073534e76c3bf9baea4872d8cac1545b
Alan Nonato da Silva <69776302+AlanNonat0@users.noreply.github.com>
2022-08-02 12:39:13 -0300

    add index file

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>deploy from bitbucket</title>
+    <style>
+        body{
+            background-color: black;
+        }
+        .container{
+            margin: 25px;
+        }
+        .container h1{
+            margin: 0 auto;
+            color: #EEE;
+        }
+        .container p{
+            margin: 0 auto;
+            color: #EEE;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Deploy from bitbucket</h1>
+        <p>
+            Paragrafo 1
+        </p>
+    </div>
+</body>
+</html>
